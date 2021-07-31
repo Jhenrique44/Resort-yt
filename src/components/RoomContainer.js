ea7358722f722ad8accf14e125019f5a33751711
Jhenrique44 <70330647+Jhenrique44@users.noreply.github.com>
2021-07-30 22:01:03 -0300

    aula4-5

---
+++ src/components/RoomContainer.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import RoomsFilter from './RoomFilter'
+import RoomsList from './RoomList'
+import { withRoomConsumer } from '../context'
+import Loading from './Loading'
+
+function RoomContainer({context}){
+    const {loading, sortedRooms, rooms} = context;
+
+    if(loading){
+        return(
+            <Loading></Loading>
+        )
+    }
+    return(
+        <>
+            <RoomsFilter rooms={rooms} />
+            <RoomsList rooms={sortedRooms} />
+        </>
+    )
+}
+export default withRoomConsumer(RoomContainer);
+// export default function RoomsContainer() {
+//     return (
+//         <RoomConsumer>
+//             {value => {
+//                 const {loading, sortedRooms, rooms} = value
+//                 if(loading){
+//                     return(
+//                         <Loading></Loading>
+//                     )
+//                 }
+//                     return(
+//                         <div> 
+//                             Hello from Rooms countainer 
+//                             <RoomsFilter rooms={rooms}></RoomsFilter>
+//                             <RoomsList rooms={sortedRooms}></RoomsList>
+//                         </div>
+//                     )
+//                 }
+//             }
+//         </RoomConsumer>
+        
+//     )
+// }
